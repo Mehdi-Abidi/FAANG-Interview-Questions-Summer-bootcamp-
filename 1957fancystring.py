44e71bfc4ac8b91ec8b33d10307806fb2fb1d111
S. M. Mehdi Abidi <mehdiali78666@gmail.com>
2025-07-21 22:15:13 +0500

    Create 1957fancystring.py

---
+++ 1957fancystring.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def makeFancyString(self, s):
+        res = []
+        for c in s:
+            if len(res) >= 2 and res[-1] == res[-2] == c:
+                continue
+            res.append(c)
+        return ''.join(res)
+        """
+        :type s: str
+        :rtype: str
+        """
+        
